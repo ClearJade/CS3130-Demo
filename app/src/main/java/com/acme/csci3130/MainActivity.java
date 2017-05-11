afc4145ce2c755a9dadb517029fd608dd1170040
ClearJade <clearjade96@hotmail.com>
2017-05-11 00:54:31 -0300

    Commit for assignment 1, some files weren't added

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Confirm button */
+    public void copyText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        TextView textToReplace = (TextView) findViewById(R.id.helloText);
+        textToReplace.setText(message);
     }
 }
